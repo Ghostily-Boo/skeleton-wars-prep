fecfdbe52ad831d1d870c7e89a42c0a7aff337a8
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-12-04 10:14:38 -0500

    Initial commit with basic folders and files setup

---
+++ lib/environment.rb
@@ -0,0 +1,8 @@
+require "skeleton/wars/version"
+
+module Skeleton
+  module Wars
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/skeletons/version.rb
@@ -0,0 +1,3 @@
+module SkeletonWars
+    VERSION = "0.1.0"
+end
