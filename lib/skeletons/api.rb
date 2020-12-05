9aec0d6795fdf7cb3d9f36a834f2605067b0301a
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-12-05 15:00:18 -0500

    api.rb plans

--- lib/skeletons/api.rb
+++ lib/skeletons/api.rb
@@ -5,9 +5,23 @@ require 'open-uri'
 class API
     BASE_URL = "http://api.wolframalpha.com/v2/query?output=JSON&appid=#{ENV['AUTH']}"
 
-    binding.pry
-    # def call
-    #     url = BASE_URL + "/species"
-    # end
+    def image_get(animal, part)
+        # base url + podid "BodyLocation:AnimalAnatomyData" + subpod "img src"
+        # animal_part = 
+    end
+
+    def plaintext_get(animal, part)
+        # BASE_URL + 
+        # "&input=#{animal}+#{part}+skeleton&podstate=100@ConstitutionalParts:AnimalAnatomyData__More"
+        # convert into list
+        # organize()
+    end
+
+    def organize()
+        # iterate over list
+        # find common words from right to left
+        # create category if none
+        # add to category count
+    end
 
 end
