31dde5ba0ae17f0b88cf2d4c0f1e7f563bb10358
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-12-08 14:36:05 -0500

    tty-prompt

--- lib/skeletons/cli.rb
+++ lib/skeletons/cli.rb
@@ -1,20 +1,14 @@
 class CLI
-    SPECIES = ["dog", "human", "horse"]
 
     def initialize
-        puts "Brush up on your skeleton knowledge!\n
-            We have information on the following species: dog, horse, human"
+        puts "Brush up on your skeleton knowledge!"
+        @prompt = TTY::Prompt.new
         species_get
     end
 
     def species_get
-        puts "Enter species name:"
-        species = gets.strip
-        #if species.lowercase matches SPECIES, FullSkeleton.new(species)
-        #returns species picture, number of bones
-        #else puts "Please enter a valid species name:"
-        puts "Please enter APPENDICULAR or AXIAL to learn more about those skeletal groups./n
-            or COMPARE [Species] to see another species' skeletal image."
+        choices = {Dog: 1, Horse: 2, Human: 3}
+        @prompt.select("Which species would you like to learn more about?", choices)
     end
 
 end
