1ca948a52c43bda0bd5bf963cf1a1a0d3b91f057
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-12-09 20:54:27 -0500

    changes to api.rb made in separate test file to preserve authentication token

--- lib/skeletons/api.rb
+++ lib/skeletons/api.rb
@@ -1,19 +1,12 @@
-require 'dotenv/load'
-
-
 class API
 
     BASE_URL = "http://api.wolframalpha.com/v2/query?output=JSON&appid=#{ENV['AUTH']}"
 
-    def initialize(animal, part)
-        image_get(animal, part)
-        plaintext_get(animal, part)
-        #or just organize(animal, part)
-    end
-
     def image_get(animal, part)
-        url = BASE_URL + "&input=#{animal}+#{part}&includepodid=BodyLocation:AnimalAnatomyData&format=image"
-        @image = JSON.load(open(url))
+        url = BASE_URL + "&input=#{animal}+#{part}&includepodid=BodyLocation:AnimalAnatomyData"
+        image = JSON.load(open(url))
+        final = result(image, "src")
+        binding.pry
     end
 
     def plaintext_get(animal, part)
@@ -22,9 +15,14 @@ class API
         binding.pry
     end
 
-    def organize()
-        #attach @image and @result to hash
-        #@result should be cleaned up
+    def result(hash, word)
+        hash.each do |key, value|
+            if key == word
+                return value
+            else
+                result(value, word)
+            end
+        end
     end
 
 end
