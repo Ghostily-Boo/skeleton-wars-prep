4ae247dfdcc9651ab1c20b720fe81f63f92f7aa0
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-12-10 18:56:11 -0500

    super messy but working temp api

--- lib/skeletons/api.rb
+++ lib/skeletons/api.rb
@@ -1,12 +1,12 @@
 class API
 
-    BASE_URL = "http://api.wolframalpha.com/v2/query?output=JSON&appid=#{ENV['AUTH']}"
+    BASE_URL = "http://api.wolframalpha.com/v2/query?output=JSON&appid=HRT7R3-8HE5552JY2"
 
     def image_get(animal, part)
         url = BASE_URL + "&input=#{animal}+#{part}&includepodid=BodyLocation:AnimalAnatomyData"
         image = JSON.load(open(url))
         binding.pry
-        result(image, "src")
+        result(image)
     end
 
     def plaintext_get(animal, part)
@@ -15,13 +15,26 @@ class API
         binding.pry
     end
 
-    def result(hash, word)
+    def result(hash)
         array = hash.values[0]["pods"]
-        array.each do |a|
-            if a.class == Hash
-                hash["src"] || hash["img"] || hash["subpods"]
+        path = ["src", "img", "subpods"]
+        def search(input, path)
+            binding.pry
+            input.each do |a|
+                binding.pry
+                if a.class == Hash
+                    binding.pry
+                    return a[path[0]] if a[path[0]]
+                    path.drop(1).each do |keyword|
+                        binding.pry
+                        search(a[keyword], path) if a[keyword]
+                    end
+                elsif a.class == Array
+                    search(a, path)
+                end
             end
         end
+        search(array, path)
     end
 
 end
