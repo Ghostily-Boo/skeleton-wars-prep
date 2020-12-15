1d21a89f026dd825823c83a7dfc58e9e3c4eac58
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-12-15 17:38:06 -0500

    skelparts metaprogramming lines

---
+++ lib/skeletons/skelparts.rb
@@ -0,0 +1,17 @@
+class SkelParts
+    @@all = []
+
+    def initialize(attributes)
+        attributes.each do |key, value| 
+            self.class.attr_accessor(key)
+            self.send(("#{key}="), value)
+        end
+        self.class.all << self
+        binding.pry
+    end
+
+    def self.all
+        @@all
+    end
+
+end
