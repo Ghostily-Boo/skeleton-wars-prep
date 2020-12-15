1d4e5cd76931d36ee30afb36bac77fedeaf72af9
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-12-15 14:44:50 -0500

    combines two search methods into one

--- lib/skeletons/api.rb
+++ lib/skeletons/api.rb
@@ -6,8 +6,8 @@ class API
         url = BASE_URL + "&input=#{animal}+#{part}&includepodid=BodyLocation:#{group}AnatomyData"
         api = JSON.load(open(url))
         array = api.values[0]["pods"]
-        path = ["src", "img", "subpods"]
-        puts search_image(array, path)
+        path = ["subpods", "img", "src"]
+        puts search(array, path)
     end
 
     def plaintext_get(animal, part, group)
@@ -22,6 +22,7 @@ class API
         count = 0
         while count <= path.length - 1
             if input.class == Array
+                input[1] if input[0] == path.last
                 input[0] == path[count] ? (input = input[1]) && (count += 1) : input = input[0]
             elsif input.class == Hash
                 input = input[path[count]]
@@ -29,19 +30,6 @@ class API
             end
         end
         input
-    end
-
-    def search_image(input, path)
-        input.each do |a|
-            if a.class == Hash
-                path.drop(1).each do |keyword|
-                    return search_image(a[keyword], path) if a[keyword]
-                end
-            elsif a.class == Array
-                return a[1] if a[0] == path[0]
-                search_image(a, path)
-            end
-        end
     end
 
 end
