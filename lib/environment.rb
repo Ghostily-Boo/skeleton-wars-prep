16be6561fdcaaf2ec88d5817e421910cdc3821d0
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-12-22 01:20:23 -0500

    removes deleted file

--- lib/environment.rb
+++ lib/environment.rb
@@ -10,5 +10,4 @@ require 'down'
 require_relative "./skeletons/api"
 require_relative "./skeletons/cli"
 require_relative "./skeletons/version"
-require_relative "./skeletons/skelparts"
 require_relative "./skeletons/fullskel"
